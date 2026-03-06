1c55b66acda777fbc2967289881f266f6d534c1a
ganesh <charawande@123>
2026-03-07 01:28:38 +0530

    Remove hardcoded ACR Role Assignment from acr.tf to fix SP Auth errors

--- infra/terraform/env/core/acr.tf
+++
@@ -1,5 +0,0 @@
-resource "azurerm_role_assignment" "aks_acr_pull" {
-  principal_id         = module.aks.kubelet_identity
-  role_definition_name = "AcrPull"
-  scope                = module.acr.acr_id
-}
