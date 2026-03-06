7d952bd747948363fd01dd6acc521f83f0ddad4b
ganesh <charawande@123>
2026-03-06 17:49:18 +0530

    security: Enforce strict Key Vault RBAC and Least Privilege

--- infra/terraform/modules/keyvault/main.tf
+++ infra/terraform/modules/keyvault/main.tf
@@ -12,6 +12,15 @@ resource "azurerm_key_vault" "kv" {
   sku_name                   = "standard"
   soft_delete_retention_days = 7
   purge_protection_enabled   = true
+  enable_rbac_authorization  = true
+}
+
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_role_assignment" "tf_kv_admin" {
+  scope                = azurerm_key_vault.kv.id
+  role_definition_name = "Key Vault Administrator"
+  principal_id         = data.azurerm_client_config.current.object_id
 }
 
 ############################################
@@ -23,6 +32,7 @@ resource "azurerm_key_vault_secret" "postgres_db" {
   name         = "postgres-db"
   value        = var.postgres_db
   key_vault_id = azurerm_key_vault.kv.id
+  depends_on   = [azurerm_role_assignment.tf_kv_admin]
 }
 
 resource "azurerm_key_vault_secret" "postgres_user" {
@@ -30,6 +40,7 @@ resource "azurerm_key_vault_secret" "postgres_user" {
   name         = "postgres-user"
   value        = var.postgres_user
   key_vault_id = azurerm_key_vault.kv.id
+  depends_on   = [azurerm_role_assignment.tf_kv_admin]
 }
 
 resource "azurerm_key_vault_secret" "postgres_password" {
@@ -37,6 +48,7 @@ resource "azurerm_key_vault_secret" "postgres_password" {
   name         = "postgres-password"
   value        = var.postgres_password
   key_vault_id = azurerm_key_vault.kv.id
+  depends_on   = [azurerm_role_assignment.tf_kv_admin]
 }
 
 resource "azurerm_key_vault_secret" "db_host" {
@@ -44,4 +56,5 @@ resource "azurerm_key_vault_secret" "db_host" {
   name         = "db-host"
   value        = var.db_host
   key_vault_id = azurerm_key_vault.kv.id
+  depends_on   = [azurerm_role_assignment.tf_kv_admin]
 }
