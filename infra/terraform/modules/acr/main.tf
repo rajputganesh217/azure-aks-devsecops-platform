877a12c143d8ebdc3969588bb9ed204664bfd8d3
ganesh <charawande@123>
2026-03-08 08:03:11 +0530

    fix: enable acr admin user to support jenkins docker push credentials

--- infra/terraform/modules/acr/main.tf
+++ infra/terraform/modules/acr/main.tf
@@ -3,5 +3,5 @@ resource "azurerm_container_registry" "acr" {
   resource_group_name = var.resource_group_name
   location            = var.location
   sku                 = "Premium"
-  admin_enabled       = false
+  admin_enabled       = true
 }
